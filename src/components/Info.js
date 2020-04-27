4d73cad1fe594800792043d30347a539433c4913
YusufKer <53224370+YusufKer@users.noreply.github.com>
2020-04-27 23:11:16 +0200

    initial commit

--- src/components/Info.js
+++ src/components/Info.js
@@ -14,7 +14,7 @@ function Info(props){
             <img src={me} alt=''/>
             <div className="info-card">
                 <h2 className='name'>Yusuf Keraan</h2>
-                <h2>Github</h2><h2>YusufKer</h2>
+                <h2>Github</h2><h2><a target="_blank" href="https://github.com/YusufKer">YusufKer</a></h2>
                 <h2>Linked In</h2><h2><a target="_blank" href='https://www.linkedin.com/in/yusuf-keraan-b962289a'>Yusuf Keraan</a></h2>
                 <h2>Email:</h2><h2>yus.ker1994@gmail.com</h2>
                 <h2>cell:</h2><h2>078 153 9093</h2> 
