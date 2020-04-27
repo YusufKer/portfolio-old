88e7c00bfe1616642b3e5d4728303bc321506796
YusufKer <53224370+YusufKer@users.noreply.github.com>
2020-04-27 23:12:13 +0200

    Updates

---
+++ precache-manifest.4048f5ae88c102a1dacf901bdea8496d.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "704ec467240629bd3a29e044cc4247df",
+    "url": "/portfolio-old/index.html"
+  },
+  {
+    "revision": "90f97d8e7223ba7ff7e7",
+    "url": "/portfolio-old/static/css/main.310d2bb7.chunk.css"
+  },
+  {
+    "revision": "df23e04c6bf9275bd4b3",
+    "url": "/portfolio-old/static/js/2.63a29d87.chunk.js"
+  },
+  {
+    "revision": "90f97d8e7223ba7ff7e7",
+    "url": "/portfolio-old/static/js/main.645acd0b.chunk.js"
+  },
+  {
+    "revision": "00a874a0bdcce7e7ca6d",
+    "url": "/portfolio-old/static/js/runtime-main.2944da69.js"
+  },
+  {
+    "revision": "1b58120f78857f0f2b8fea1da1e0ea6d",
+    "url": "/portfolio-old/static/media/ExclusiveBooks.1b58120f.jpg"
+  },
+  {
+    "revision": "58208a64a36f9501dc691eecff5a76b1",
+    "url": "/portfolio-old/static/media/ProfilePic.58208a64.jpg"
+  },
+  {
+    "revision": "f87519ff832c9fe0af7d68bebb45a593",
+    "url": "/portfolio-old/static/media/bladesAndTriggers.f87519ff.jpg"
+  },
+  {
+    "revision": "30ac1deb1a2c08cc263fedef318f5a5d",
+    "url": "/portfolio-old/static/media/calculator.30ac1deb.png"
+  },
+  {
+    "revision": "665e2f0a4e762d7f9eba26d2568fb928",
+    "url": "/portfolio-old/static/media/lightbox.665e2f0a.png"
+  },
+  {
+    "revision": "715c1786b669dbf5db2cb2f64dc1db47",
+    "url": "/portfolio-old/static/media/school.715c1786.jpg"
+  },
+  {
+    "revision": "dbea02b74e3e09d3c3c2f631b3d11810",
+    "url": "/portfolio-old/static/media/task10.dbea02b7.png"
+  },
+  {
+    "revision": "1a9990af26570440c7e4478433ee23f5",
+    "url": "/portfolio-old/static/media/task15.1a9990af.png"
+  },
+  {
+    "revision": "849f48ad359cfc18fbf0ae4e684b5e86",
+    "url": "/portfolio-old/static/media/weatherApp.849f48ad.png"
+  }
+]);
